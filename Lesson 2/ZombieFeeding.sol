c928f07e85873d6df82d9045fd15a5ba0f8cd413
Jordan <jordanonuma@outlook.com>
2018-09-08 22:52:06 -1000

    Declared local  struct named  (storage pointer)--all in created function feedAndMultiply.

--- Lesson 2/ZombieFeeding.sol
+++ Lesson 2/ZombieFeeding.sol
@@ -1,7 +1,19 @@
 pragma solidity ^0.4.19;
 
+import "./ZombieFactory.sol";
 
 // ZombieFeeding.sol inherits from ZombieFactory.sol (previously Contract.sol)
 contract ZombieFeeding is ZombieFactory {
 
+
+
+function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+  
+  // Makes sure sender is this zombie's owner.
+  require(msg.sender == zombieToOwner[_zombieId]);
+  // Declares local `Zombie` named `myZombie` (storage pointer).
+  // Sets variable to be equal to index _zombieId in our `zombies` array.
+  Zombie storage myZombie = zombies[_zombieId];
+
+}
 }

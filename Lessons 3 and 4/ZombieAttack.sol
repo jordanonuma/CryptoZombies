ef9568b46cf57931f6d996dc5fd130f3333c8a29
Jordan <jordanonuma@outlook.com>
2018-09-22 10:13:37 -1000

    Added function randMod() whose pseudo-random number generated will be used to determine zombie battle outcomes.

--- Lessons 3 and 4/ZombieAttack.sol
+++ Lessons 3 and 4/ZombieAttack.sol
@@ -3,7 +3,15 @@ import "./ZombieHelper.sol";
 
 contract ZombieBattle is ZombieHelper {
 
-  // A random number is used to determine zombie battle outcomes.
-  
+  uint randNonce = 0;
+
+  // Function randMond() creates a random number that will be used to determine
+  // zombie battle outcomes.
+  function randMod(uint _modulus) internal returns(uint) {
+    randNonce++;
+
+    // keccak256 takes  hash of now, msg.sender, and randNonce.
+    return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
+  }
 
 } // end Contract ZombieBattle {}

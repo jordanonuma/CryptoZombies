85c571553decb954d22b99d538399244a579c7ed
Jordan <jordanonuma@outlook.com>
2018-09-23 10:00:33 -1000

    Added function attack() header and set attacking zombie's pre-set 70% probability of winning.

--- Lessons 3 and 4/ZombieAttack.sol
+++ Lessons 3 and 4/ZombieAttack.sol
@@ -4,6 +4,7 @@ import "./ZombieHelper.sol";
 contract ZombieBattle is ZombieHelper {
 
   uint randNonce = 0;
+  uint attackVictoryProbability = 70; // Attacking zombie has a 70% chance of winning.
 
   // Function randMond() creates a random number that will be used to determine
   // zombie battle outcomes.
@@ -12,6 +13,14 @@ contract ZombieBattle is ZombieHelper {
 
     // keccak256 takes  hash of now, msg.sender, and randNonce.
     return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
-  }
+    
+  } // end of function randMod()
+
+  // If the attacking zombie wins, it levels up and spawns a new zombie.
+  // If it loses, nothing happens (except its lossCount incrementing).
+  // Whether it wins or loses, the attacking zombie's cooldown time will begin.
+  function attack(uint _zombieId, uint _targetId) external {
+
+  } // end of function attack()
 
 } // end Contract ZombieBattle {}

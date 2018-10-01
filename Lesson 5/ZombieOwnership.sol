edb9131e69bd649b4c106e6a552160fd001bb10b
Jordan <jordanonuma@outlook.com>
2018-09-30 18:59:14 -1000

    Added ownerZombieCount[] and zombieToOwner[] mappings to function balanceOf() and ownerOf, respectively, to Contract ZombieOwnership {}.

--- Lesson 5/ZombieOwnership.sol
+++ Lesson 5/ZombieOwnership.sol
@@ -5,4 +5,25 @@ import "./ERC721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
+  function balanceOf(address _owner) public view returns (uint256 _balance) {
+    // ownerZombieCount[] mapping is from Contract ZombieFactory {}.
+    return ownerZombieCount[_owner];
+  }
+
+  function ownerOf(uint256 _tokenId) public view returns (address _owner) {
+    // zombieToOwner[] mapping is from Contract ZombieFactory {}.
+    return zombieToOwner[_tokenId];
+  }
+
+  function transfer(address _to, uint256 _tokenId) public {
+
+  }
+
+  function approve(address _to, uint256 _tokenId) public {
+
+  }
+
+  function takeOwnership(uint256 _tokenId) public {
+
+  }
 }

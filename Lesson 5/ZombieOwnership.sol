7c3e829784e7936d2a6ff9d38e7d21ef8afb2012
Jordan <jordanonuma@outlook.com>
2018-10-03 05:17:35 -1000

    Added public function transfer() callable only by msg.sender. Public function transfer() calls private function _transfer(), which increments and decrements zombie counts along with transferring zombie ERC721 token.

--- Lesson 5/ZombieOwnership.sol
+++ Lesson 5/ZombieOwnership.sol
@@ -13,7 +13,7 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
   function ownerOf(uint256 _tokenId) public view returns (address _owner) {
     // zombieToOwner[] mapping is from Contract ZombieFactory {}.
     return zombieToOwner[_tokenId];
-  }
+  } // end of function ownerOf()
 
   function _transfer(address _from, address _to, uint256 _tokenId) private {
       ownerZombieCount[_to]++;
@@ -21,15 +21,18 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
 
       // Changes the zombieToOwner mapping [from Contract ZombieFactory {}] for this
       // so for every '_tokenId' zombie, the mapping shows '_to' owner.
-      zombieToOwner[_tokenId] => _to;
+      zombieToOwner[_tokenId] = _to;
 
       // Fires 'Event Transfer' in ERC721.sol
       Transfer(_from, _to, _tokenId);
-  } // end of function _transfer()
+  } // end of function _transfer() private {}
 
-  function transfer(address _to, uint256 _tokenId) public {
-
-  }
+  function transfer(address _to, uint256 _tokenId) onlyOwnerOf public {
+    // Public function transfer() is restricted to msg.sender.
+    // transfer() calls private function _transfer(), restricted to callable by
+    // functions within this contract.
+    _transfer(msg.sender, _to, _tokenId);
+  } // end of function transfer() public {}
 
   function approve(address _to, uint256 _tokenId) public {
 
